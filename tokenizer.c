24c5eb5040e50086bf7536e9dbf14d6fdcf26ab5
6felix <felix.okoronkwo.1@gmail.com>
2023-07-29 16:38:48 +0100

    tokenizer now functional to parse opcode and its value from file but not non value opcode syntaxes

--- tokenizer.c
+++ tokenizer.c
@@ -25,26 +25,41 @@ int parser (FILE *file)
 	int opcode_start = -1, opcode_end = -1;
 	int value_start = -1, value_end = -1;
 	char *opcode , *value;
-	command current;
+	int opcode_len; int value_len;
+	command current_command;
 
 	while (getline(&line_content, &len, file) != EOF)
 	{
 		index = tokenizer(line_content, index, &opcode_start, &opcode_end, is_alpha);
 		index = tokenizer(line_content, index, &value_start, &value_end, is_number);
 
-		opcode = malloc((opcode_end - opcode_start) + 2);
-		value = malloc((value_end - value_start) + 2);
+		opcode_len = (opcode_end - opcode_start) + 1;
+		value_len = (value_end - value_start) + 1;
+
+		opcode = malloc(opcode_len + 1);
+		value = malloc(value_len + 1);
 		if (!opcode || !value)
 			malloc_error();
 
-		printf("\nopcode_start: %d, opcode_end: %d\n", opcode_start, opcode_end);
-		printf("value_start: %d, value_end: %d\n", value_start, value_end);
+		index = 0;
+		while (index < opcode_len)
+			opcode[index++] = line_content[opcode_start++];
+		opcode[index] = '\0';
+
+		index = 0;
+		while (index < value_len)
+			value[index++] = line_content[value_start++];
+		value[index] = '\0';
+
+		current_command.value = atoi(value);
+		current_command.opcode = opcode;
+
+		printf("[%d]: opcode: %s, value: %d\n",line_counter, current_command.opcode, current_command.value);
 
 		/** reset indexes for next line **/
-		opcode_start = opcode_end = value_end = value_start =  index = 0;
+		opcode_start = opcode_end = value_end = value_start = -1;
+		index = 0;
 		line_counter++;
 	}
-
-	putchar((char) 10);
 	return (EXIT_SUCCESS);
 }
