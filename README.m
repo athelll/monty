6f8037d65b67e4538bd62011a2dc5610332e14d6
felixhatesbugs <110993465+felixhatesbugs@users.noreply.github.com>
2023-07-30 07:15:52 +0100

    Update README.m

--- README.m
+++ README.m
@@ -12,9 +12,17 @@ push 2
 push 3
 pall
   
+                                                                            MONTY 
 
-
-                        
+                       
 
 push 4
   shape 10
+
+vulnerabilities i just noticed:
+1) since the tokenizer function uses its own initialized index variable unlike before that after tokenizing the opcode it updates the scoped index valriable so when tokenizing the number value its starts from when the opcode ends in the line of the file its reading from
+there-by making reading faster. but in the current implementation its doesn't do that and read the line its trying to tokenize twice to get the opcode and then the value:
+   pros:
+   1) it can find if a line began with a number and can determine the next action to do with that ( throw error? )
+   cons:
+   1) theoretically slower but almost unoticeable.
